92eed30912b2dbd93c11bc26fe07002f9ced5557
galogico <galogico@gmail.com>
2023-03-07 15:42:27 -0300

    fazendo o modal do carrinho de compras

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
 import Header from "./components/Layout/Header"
 import Meals from "./components/Meals/meals"
 import Cart from "./components/Cart/cart"
+import { useState } from "react"
 
 export const App = ()=>{
+    const [cartIsShown,setCartIsShown] = useState(false);
+
+    const showCartHandler = ()=>{
+        setCartIsShown(true);
+    }
+
+    
+    const hideCartHandler = ()=>{
+        setCartIsShown(false);
+    }
     return(
         <>
-        <Cart></Cart>
-        <Header></Header>
+        { cartIsShown  && <Cart onClose={hideCartHandler}/>}
+        <Header
+            onShowCart={showCartHandler}
+            
+        />
         <Meals/>
         </>
 
--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -11,7 +11,7 @@ const Header = props =>{
                     Jacquin's gourmet buffet
                 </h1>
                 
-                <HeaderCartButton/>
+                <HeaderCartButton onShowCart={props.onShowCart}/>
                 
             </header>
             <div className={classes['main-image']}>
