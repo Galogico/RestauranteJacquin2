a0cc575d1f4afecf882c2edf45fc812baf7a3004
galogico <galogico@gmail.com>
2023-03-21 16:33:00 -0300

    finalizando a aba do cart

--- src/components/Cart/cart.jsx
+++ src/components/Cart/cart.jsx
@@ -2,16 +2,26 @@ import classes from "./Cart.module.css";
 import Modal from "../UI/modal";
 import { useContext } from "react";
 import cartContext from '../../store/cart-context'
+import CartItem from "./CartItem";
 
 const Cart =props=>{
     const cartCtx = useContext(cartContext);
     const totalAmount = `R$${cartCtx.totalAmount.toFixed(2)}`;
 
     const hasItems = cartCtx.items.length > 0;
+    const cartItemRemoveHandler = id=>{};
+    const cartItemAddHandler = item =>{};
     const cartItems =(
         <ul className = {classes['cart-items']}>
             {
-                cartCtx.items.map((item)=> <li key={item.id}>{item.name}</li>)
+                cartCtx.items.map((item)=> <CartItem 
+                key={item.id}
+                name={item.name}
+                amount={item.amount}
+                price={item.price}
+                onRemove={cartItemRemoveHandler.bind(null,item.id)}
+                onAdd={cartItemAddHandler.bind(null,item)}
+                />)
             }
         </ul>
     )
